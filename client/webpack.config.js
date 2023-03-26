54bb5492315be0628f2307a21faa925ba380ed7b
FrankM89 <114839168+FrankM89@users.noreply.github.com>
2023-03-25 20:02:50 -0700

    Webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,30 +1,74 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
-const path = require('path');
-const { InjectManifest } = require('workbox-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const path = require("path");
+const { InjectManifest } = require("workbox-webpack-plugin");
+// const { header } = require('./src/js/header');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+module.exports = {
+  mode: "development",
+  entry: {
+    // databases:'./src/js/database.js',
+    // editor: './src/js/editor.js',
+    // header: './src/js/header.js',
+    main: "./src/js/index.js",
+    install: "./src/js/install.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 
-module.exports = () => {
-  return {
-    mode: 'development',
-    entry: {
-      main: './src/js/index.js',
-      install: './src/js/install.js'
-    },
-    output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-    },
-    plugins: [
-      
-    ],
-
-    module: {
-      rules: [
-        
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+      title: "Webpack Plugin",
+    }),
+    new MiniCssExtractPlugin(),
+    new WebpackPwaManifest({
+      name: "My App",
+      short_name: "App",
+      inject: true,
+      start_url: "/",
+      publicPath: "/",
+      description: "My awesome app",
+      icons: [
+        {
+          src: path.resolve("src/images/logo.png"),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join("assets", "icons"),
+        },
       ],
-    },
-  };
+    }),
+    new InjectManifest({
+      swSrc: "/src-sw.js",
+      swDest: "src-sw.js",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+            plugins: [
+              "@babel/plugin-proposal-object-rest-spread",
+              "@babel/transform-runtime",
+            ],
+          },
+        },
+      },
+    ],
+  },
 };
